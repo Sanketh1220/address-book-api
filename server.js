ca103e2bf02840020a4cf687ffa917eafa6dc015
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-06-09 00:48:00 +0530

    feat: app is connected to database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const databaseConnection = require('./config/dbConfig');
+
+databaseConnection();
 
 app.use(express.urlencoded({
     extended: true
